5b74fc571ca345c3c87068f15eb87e087636f19d
Dmitry <145367127+dmitrychervochkin@users.noreply.github.com>
2025-04-03 12:27:33 +0300

    Update getPosts.ts

--- src/api/operations/getPosts.ts
+++ src/api/operations/getPosts.ts
@@ -1,11 +1,12 @@
 export const getPosts = async () => {
     const posts = await fetch("http://server-production-61e7.up.railway.app/posts", {
-  method: 'GET',
+  method: 'OPTIONS',
   headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   },
-    }).then((data) =>
+}).then((data) =>
         data.json()
     )
 
