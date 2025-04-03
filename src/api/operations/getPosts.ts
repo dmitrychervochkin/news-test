d5a2f634dd8bdfeeed4e43e7e6fafab55ecb872b
Dmitry <145367127+dmitrychervochkin@users.noreply.github.com>
2025-04-03 12:42:08 +0300

    Update getPosts.ts

--- src/api/operations/getPosts.ts
+++ src/api/operations/getPosts.ts
@@ -1,5 +1,11 @@
 export const getPosts = async () => {
-    const posts = await fetch('/api/posts').then((data) =>
+    const posts = await fetch('https://server-production-61e7.up.railway.app/posts', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  mode: 'cors',  // 👈 Обязательно
+}).then((data) =>
         data.json()
     )
 
