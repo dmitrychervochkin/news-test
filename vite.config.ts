d96c4ed9159863fc52551242f896346ea572ee7f
Dmitry <145367127+dmitrychervochkin@users.noreply.github.com>
2025-04-03 12:42:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://server-production-61e7.up.railway.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  
 })
