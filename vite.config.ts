35c39d753e187835d97116695d05fabaeb9cf613
Dmitry <chervochkin2001@mail.ru>
2025-04-03 13:08:28 +0300

    qweq

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react()],
   base: './'
-=======
-    plugins: [react()],
-    base: "./",
->>>>>>> 1daa860 (qweqwe)
 })
